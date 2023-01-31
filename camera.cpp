5bd1b2fe7ec54a9bb6ea584b8a271ac785a8cfca
Jeremiah Hawthorne <85040089+jeremiah77923@users.noreply.github.com>
2023-01-31 10:40:37 -0600

    Created camera class to process and read images.

---
+++ camera.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jeremiah Hawthorne on 1/31/23.
+//
+
+#include "camera.h"
---
+++ camera.h
@@ -0,0 +1,14 @@
+//
+// Created by Jeremiah Hawthorne on 1/31/23.
+//
+
+#ifndef SELFDRIVINGCAR_CAMERA_H
+#define SELFDRIVINGCAR_CAMERA_H
+
+
+class camera {
+
+};
+
+
+#endif //SELFDRIVINGCAR_CAMERA_H
